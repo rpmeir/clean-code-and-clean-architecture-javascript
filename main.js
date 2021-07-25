8080811faf5db291856224e1854acd4968627d1c
Rodrigo Pires Meira <rpmeir@gmail.com>
2021-07-25 14:12:19 -0300

    Step 1 - Refactor main function

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const validateCpfBefore = require("./validateCpf_before");
 const validateCpfAfter = require("./validateCpf_after");
 
-console.log('\n - Before');
-console.log(validateCpfBefore.validate("00000000000"));
-console.log(validateCpfBefore.validate("86446422799"));
-console.log(validateCpfBefore.validate("86446422784"));
-console.log(validateCpfBefore.validate("864.464.227-84"));
-console.log(validateCpfBefore.validate("91720489726"));
-console.log(validateCpfBefore.validate("a1720489726"));
-console.log('\n - After');
-console.log(validateCpfAfter.validate("00000000000"));
-console.log(validateCpfAfter.validate("86446422799"));
-console.log(validateCpfAfter.validate("86446422784"));
-console.log(validateCpfAfter.validate("864.464.227-84"));
-console.log(validateCpfAfter.validate("91720489726"));
-console.log(validateCpfAfter.validate("a1720489726"));
+const cpf1 = '00000000000';
+const cpf2 = '86446422799';
+const cpf3 = '86446422784';
+const cpf4 = '864.464.227-84';
+const cpf5 = '91720489726';
+const cpf6 = 'a1720489726';
+
+console.log('\t\t', 'Before', ' After');
+console.log(`"${cpf1}"`, '\t', validateCpfBefore.validate(`${cpf1}`), '\t', validateCpfAfter.validateCpf(`${cpf1}`));
+console.log(`"${cpf2}"`, '\t', validateCpfBefore.validate(`${cpf2}`), '\t', validateCpfAfter.validateCpf(`${cpf2}`));
+console.log(`"${cpf3}"`, '\t', validateCpfBefore.validate(`${cpf3}`), '\t', validateCpfAfter.validateCpf(`${cpf3}`));
+console.log(`"${cpf4}"`, validateCpfBefore.validate(`${cpf4}`), '\t', validateCpfAfter.validateCpf(`${cpf4}`));
+console.log(`"${cpf5}"`, '\t', validateCpfBefore.validate(`${cpf5}`), '\t', validateCpfAfter.validateCpf(`${cpf5}`));
+console.log(`"${cpf6}"`, '\t', validateCpfBefore.validate(`${cpf6}`), '\t', validateCpfAfter.validateCpf(`${cpf6}`));
