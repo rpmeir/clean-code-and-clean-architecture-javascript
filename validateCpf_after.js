93072d14f2e5757117125c4bee57fe78a5e3a7fb
Rodrigo Pires Meira <rpmeir@gmail.com>
2021-07-25 23:01:55 -0300

    Step 1 - Refactor main function

--- validateCpf_after.js
+++ validateCpf_after.js
@@ -1,61 +1,38 @@
-function isNullOrUndefined(str) {
-	if (str === null || str === undefined) {
-		return false;
-	}
-}
 
 function removeNotNumbers(str) {
 	return str.replace(/\D/g, '');
 }
 
-function hasCorrectLength(str) {
-	if (str.length !== 11) {
-		return false;
-	}
+function hasInvalidLength(str) {
+	return str.length !== 11;
 }
 
 function validateCpf(str) {
-
-	isNullOrUndefined(str);
+	if(!str) return false;
 	str = removeNotNumbers(str);
-	hasCorrectLength(str);
-
-	if (!str.split("").every(c => c === str[0])) {
-		try {
-			let d1, d2;
-			let dg1, dg2, rest;
-			let digito;
-			let nDigResult;
-			d1 = d2 = 0;
-			dg1 = dg2 = rest = 0;
-
-			for (let nCount = 1; nCount < str.length - 1; nCount++) {
-				// if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-				// 	return false;
-				// } else {
-
-				digito = parseInt(str.substring(nCount - 1, nCount));
-				d1 = d1 + (11 - nCount) * digito;
-				d2 = d2 + (12 - nCount) * digito;
-				// }
-			};
-
-			rest = (d1 % 11);
-			dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-			d2 += 2 * dg1;
-			rest = (d2 % 11);
-			if (rest < 2)
-				dg2 = 0;
-			else
-				dg2 = 11 - rest;
-			let nDigVerific = str.substring(str.length - 2, str.length);
-			nDigResult = "" + dg1 + "" + dg2;
-			return nDigVerific == nDigResult;
-		} catch (e) {
-			console.error("Erro !" + e);
-			return false;
-		}
-	} else return false;
+	if (hasInvalidLength(str)) return false;
+	if (str.split("").every(c => c === str[0])) return false;
+	let d1 = 0;
+	let d2 = 0;
+	let dg1 = 0;
+	let dg2 = 0;
+	let rest = 0;
+	for (let nCount = 1; nCount < str.length - 1; nCount++) {
+		const digito = parseInt(str.substring(nCount - 1, nCount));
+		d1 = d1 + (11 - nCount) * digito;
+		d2 = d2 + (12 - nCount) * digito;
+	};
+	rest = (d1 % 11);
+	dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+	d2 += 2 * dg1;
+	rest = (d2 % 11);
+	if (rest < 2)
+		dg2 = 0;
+	else
+		dg2 = 11 - rest;
+	let nDigVerific = str.substring(str.length - 2, str.length);
+	let nDigResult = "" + dg1 + "" + dg2;
+	return nDigVerific == nDigResult;
 }
 
 module.exports = {
