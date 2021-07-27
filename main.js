fe552dc13ab55298eb982b068107c0fd00491e54
Rodrigo Pires Meira <rpmeir@gmail.com>
2021-07-27 00:42:23 -0300

    Step 1 - Final refactor main function

--- main.js
+++ main.js
@@ -12,6 +12,6 @@ console.log('\t\t', 'Before', ' After');
 console.log(`"${cpf1}"`, '\t', validateCpfBefore.validate(`${cpf1}`), '\t', validateCpfAfter.validateCpf(`${cpf1}`));
 console.log(`"${cpf2}"`, '\t', validateCpfBefore.validate(`${cpf2}`), '\t', validateCpfAfter.validateCpf(`${cpf2}`));
 console.log(`"${cpf3}"`, '\t', validateCpfBefore.validate(`${cpf3}`), '\t', validateCpfAfter.validateCpf(`${cpf3}`));
-console.log(`"${cpf4}"`, validateCpfBefore.validate(`${cpf4}`), '\t', validateCpfAfter.validateCpf(`${cpf4}`));
+console.log(`"${cpf4}"`,       validateCpfBefore.validate(`${cpf4}`), '\t', validateCpfAfter.validateCpf(`${cpf4}`));
 console.log(`"${cpf5}"`, '\t', validateCpfBefore.validate(`${cpf5}`), '\t', validateCpfAfter.validateCpf(`${cpf5}`));
 console.log(`"${cpf6}"`, '\t', validateCpfBefore.validate(`${cpf6}`), '\t', validateCpfAfter.validateCpf(`${cpf6}`));
